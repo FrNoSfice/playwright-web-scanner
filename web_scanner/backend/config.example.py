0ae63763343ed658125e1a14eda78be9911406ae
FrNoSfice <131359120+FrNoSfice@users.noreply.github.com>
2026-04-26 14:05:03 +0800

    Delete web_scanner directory

--- web_scanner/backend/config.example.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-MYSQL_HOST = '127.0.0.1'
-MYSQL_PORT = 3306
-MYSQL_USER = 'root'       #改成你自己的数据库用户名
-MYSQL_PASSWORD = 'root'   # 改成你自己的数据库密码
-MYSQL_DB = 'webscan'      #改成你自己的数据库名
-MYSQL_CHARSET = 'utf8mb4'
